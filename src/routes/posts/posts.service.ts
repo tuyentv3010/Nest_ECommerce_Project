13b2dd6bb98b53fb33e81d6889b3b04a45eeb624
tuyen <tuyenbest1234@gmail.com>
2025-05-22 00:00:25 +0700

    create config.ts to check type .env and suggest code

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import envConfig from 'src/shared/config';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
   getPost() {
+    console.log(envConfig.DATABASE_URL);
+    console.log(envConfig.ACCESS_TOKEN_SECRET);
     return this.prismaService.post.findMany();
   }
   createPost(body: any) {
-    return body;
+    const userId = 1;
+    return this.prismaService.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userId,
+      },
+    });
   }
 }
---
+++ src/shared/config.ts
@@ -0,0 +1,46 @@
+import { constants } from 'buffer';
+import { plainToInstance } from 'class-transformer';
+import { IsString, validateSync } from 'class-validator';
+import fs from 'fs';
+import path from 'path';
+import { config } from 'dotenv';
+config({
+  path: '.env',
+});
+// Check already have .env??
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('Not found file .env');
+  process.exit(1);
+}
+
+class ConfigSchema {
+  @IsString()
+  DATABASE_URL: string;
+  @IsString()
+  ACCESS_TOKEN_SECRET: string;
+  @IsString()
+  ACCESS_TOKEN_EXPIRES_IN: string;
+  @IsString()
+  REFRESH_TOKEN_SECRET: string;
+  @IsString()
+  REFRESH_TOKEN_EXPIRES_IN: string;
+}
+const configServer = plainToInstance(ConfigSchema, process.env, {
+  enableImplicitConversion: true,
+});
+const error = validateSync(configServer);
+console.log(configServer);
+console.log(error);
+if (error.length > 0) {
+  console.log('Cac gia tri khai bao trong .env khong hop le');
+  const errors = error.map((eItem) => {
+    return {
+      property: eItem.property,
+      constants: eItem.constraints,
+      value: eItem.value,
+    };
+  });
+  throw errors;
+}
+const envConfig = configServer;
+export default envConfig;
